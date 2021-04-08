7291433b896aea27368498a0842c4b7e1c655925
FlorianClanet <florianclanet@gmail.com>
2021-04-09 00:38:05 +0200

    feat|Retrieve counter value from cosmosDB

--- back/GetResumeUsersCounter/__init__.py
+++ back/GetResumeUsersCounter/__init__.py
@@ -6,27 +6,12 @@ from azure.cosmos import exceptions, CosmosClient, PartitionKey
 def main(req: func.HttpRequest, inputDocument: func.DocumentList) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    test = inputDocument
-    
-    query = "SELECT * FROM c WHERE c.lastName IN ('Wakefield', 'Andersen')"
-    items = list(container.query_items(
-        query=query,
-        enable_cross_partition_query=True
-    ))
-    
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    counter = inputDocument[0]['usersCounter']
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    if counter:
+        return func.HttpResponse(f"{counter}", status_code=200)
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+             "Error",
+             status_code=500
         )
