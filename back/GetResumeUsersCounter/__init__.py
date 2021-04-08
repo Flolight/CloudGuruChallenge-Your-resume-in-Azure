1a42e8a67aa19b8905a1c20f4d951162c075e807
FlorianClanet <florianclanet@gmail.com>
2021-04-09 01:29:28 +0200

    feat| Increment counter (WIP)

--- back/GetResumeUsersCounter/__init__.py
+++ back/GetResumeUsersCounter/__init__.py
@@ -3,11 +3,13 @@ import logging
 import azure.functions as func
 from azure.cosmos import exceptions, CosmosClient, PartitionKey
 
-def main(req: func.HttpRequest, inputDocument: func.DocumentList) -> func.HttpResponse:
+def main(req: func.HttpRequest, inputDocument: func.DocumentList, outputDoc: func.Out[func.Document]) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    counter = inputDocument[0]['usersCounter']
-
+    counter = inputDocument[0]['usersCounter'] + 1
+    inputDocument[0]['usersCounter'] = counter
+    test = inputDocument[0].to_json()
+    outputDoc.set(func.Document.from_json(inputDocument[0].to_json()))
     if counter:
         return func.HttpResponse(f"{counter}", status_code=200)
     else:
