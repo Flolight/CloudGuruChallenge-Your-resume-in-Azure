75764f859d9da26169c07f80f865eb60c03b2c2c
FlorianClanet <florianclanet@gmail.com>
2021-04-14 00:55:14 +0200

    fix|remove old contact wow stuff

--- front/js/custom.js
+++ front/js/custom.js
@@ -136,17 +136,3 @@ $(document).ready(function() {
 
    $(".fancybox").fancybox();
 });
-
-wow = new WOW({
-    animateClass: 'animated',
-    offset: 100
-});
-wow.init();
-document.getElementById('').onclick = function() {
-    var section = document.createElement('section');
-    section.className = 'wow fadeInDown';
-    section.className = 'wow shake';
-    section.className = 'wow zoomIn';
-    section.className = 'wow lightSpeedIn';
-    this.parentNode.insertBefore(section, this);
-};
